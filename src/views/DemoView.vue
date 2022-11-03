29e87c095717b99cb7cfd9b352b327e46945d3bd
VueJSProg <JavascriptVue@outlook.com>
2022-11-03 15:00:45 +0100

    Master

---
+++ src/views/DemoView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="special">
+        // put HTML here
+        <br>
+        <button @click="methodClicked()">ButtonClicked</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DemoView',
+    components: {
+    },
+    props: {
+    },
+    methods: {
+        methodClicked() {
+            alert('You have clicked the button')
+        },
+    },
+    mounted() {
+        console.log('Demo mounted') // 0
+    }
+}
+</script>
+
+<style>
+#special {
+    text-decoration: line-through;
+}
+/* Put the additionnal style here  */
+</style>
